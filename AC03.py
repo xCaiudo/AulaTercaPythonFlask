468575c92c5eb1c7ccfd79ee2b11453cb8af7cf6
Caio <xCaiudo16@gmail.com>
2021-09-14 21:44:00 -0300

    teste commit

--- AC03.py
+++ AC03.py
@@ -1,3 +1,35 @@
 from unittest import TestCase, main
 import abc
 
+class Caculadora (object):
+    def calcular (self, valor1, valor2, operador):
+        operacaoFabrica = operacaoFabrica ()
+        operacao= operacaoFabrica.qualOperacao(operador.lower())
+        if (operacao == None):
+            return 0
+        else:
+            resultado = coperacao.executar(valor1, valor2)
+            return resultado
+
+class OperacaoFabrica (object):
+    def qualOperacao(self, operador):
+        if (operador == "soma"):
+            return Soma()
+        elif (operador == "subtracao"):
+            return Subtracao()
+        elif (operador == "divisao"):
+            return Divisao()
+        elif (operador == "multiplicacao"):
+            return Mutiplicacao()
+
+class Operacao(metaclass=abc.ABCMeta):
+    
+    @abc.abstractmethod
+    def executar(self, valor1, valor2):
+        pass
+
+class Soma(Operacao):
+    resultado = valor1 + valor2
+    return resultado
+
+    
